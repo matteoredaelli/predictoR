bf416e08058ea6d9d10b2462e9071f0819c0ccd6
Matteo Redaelli <matteo@ubuntu.(none)>
2012-10-30 22:52:27 +0100

    first upload

---
+++ config.R
@@ -0,0 +1,16 @@
+library(ltp)
+dir.create(file.path(tempdir(),'plots'),showWarnings=FALSE)
+app <- Builder$new(
+    Static$new(
+	urls = c('/css','/js', '/img'),
+	root = 'bootstrap'
+    ),
+    Static$new(urls='/plots',root=tempdir()),
+    Brewery$new(
+	url='/brew',
+	root='.',
+	imagepath=file.path(tempdir(),'plots'),
+	imageurl='../plots/'
+    ),
+    Redirect$new('/brew/index.rhtml')
+)
---
+++ rook-start.R
@@ -0,0 +1,37 @@
+#/usr/bin/env Rscript
+
+library(Rook)
+source("config.R")
+
+myPort <- 8000
+myInterface <- "0.0.0.0"
+status <- -1
+
+# R 2.15.1 uses .Internal, but the next release of R will use a .Call.
+# Either way it starts the web server.
+if (as.integer(R.version[["svn rev"]]) > 59600) {
+    status <- .Call(tools:::startHTTPD, myInterface, myPort)
+} else {
+    status <- .Internal(startHTTPD(myInterface, myPort))
+}
+
+if (status == 0) {
+    unlockBinding("httpdPort", environment(tools:::startDynamicHelp))
+    assign("httpdPort", myPort, environment(tools:::startDynamicHelp))
+
+    s <- Rhttpd$new()
+    s$listenAddr <- myInterface
+    s$listenPort <- myPort
+
+    # Change this line to your own application. You can add more than one
+    # application if you like
+    s$add(name="predictoR",
+          app=app
+          )
+    # Now make the console go to sleep. Of course the web server will still be
+    # running.
+    while (TRUE) Sys.sleep(24 * 60 * 60)
+}
+
+# If we get here then the web server didn't start up properly
+warning("Oops! Couldn't start Rook app")
